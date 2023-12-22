821f9229991433e6afd3c793d18787375ad4d096
Lazar Zlatiev <lazar.zlatiev@rebtel.com>
2023-12-22 11:16:18 +0000

    version1

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Binocs
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
